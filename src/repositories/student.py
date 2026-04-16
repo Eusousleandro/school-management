76b7162a1d45058591175ad46e47e718ae70e34e
Leandro Sousa Silva <ordnael.sousass@gmail.com>
2026-04-16 00:25:06 -0300

    Modifitation in repository student.

--- src/repositories/student.py
+++ src/repositories/student.py
@@ -8,30 +8,30 @@ class StudentRepository:
         return db.query(Student).all()
 
     @staticmethod
-    async def get_student_id(self, db: Session, id: int):
+    async def get_student_id(db: Session, id: int):
         return db.query(Student).filter(Student.id == id).first()
     
     @staticmethod
-    async def get_student_email(self, db: Session, email: str):
+    async def get_student_email(db: Session, email: str):
         return db.query(Student).filter(Student.email == email).first()
     
     @staticmethod
-    async def get_student_cpf(self, db: Session, cpf: str):
+    async def get_student_cpf(db: Session, cpf: str):
         return db.query(Student).filter(Student.cpf == cpf).first()
     
     @staticmethod
-    async def get_student_rg(self, db: Session, rg: str):
+    async def get_student_rg(db: Session, rg: str):
         return db.query(Student).filter(Student.rg == rg).first()
 
     @staticmethod
-    async def create_student(self, db: Session, student: Student):
+    async def create_student(db: Session, student: Student):
         new_student = Student(**student.dict())
         db.add(new_student)
         db.commit()
         db.refresh(new_student)
         return new_student
 
-    async def update_student(self, db: Session, id: int, student: Student):
+    async def update_student(db: Session, id: int, student: Student):
         up_student = db.query(Student).filter(Student.id == id).first()
         update_data = student.dict(exclude_unset=True)
 
@@ -43,7 +43,7 @@ class StudentRepository:
         return up_student
     
     @staticmethod
-    async def delete_student(self, db: Session, id: int):
+    async def delete_student(db: Session, id: int):
         student_delete = db.query(Student).filter(Student.id == id).first()
         db.delete(student_delete)
         db.commit()
