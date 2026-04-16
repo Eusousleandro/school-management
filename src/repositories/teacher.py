3d03cdd0a4d388504237cdeef3c9f802d7c565c0
Leandro Sousa Silva <ordnael.sousass@gmail.com>
2026-04-16 00:23:03 -0300

    Modifitation in repository teahcer.

--- src/repositories/teacher.py
+++ src/repositories/teacher.py
@@ -4,15 +4,15 @@ from models.teacher import Teacher
 class TeacherRepository: 
 
     @staticmethod
-    async def get_teachers(self, db: Session):
+    async def get_teachers(db: Session):
         return db.query(Teacher).all()
     
     @staticmethod
-    async def get_teacher_id(self, db: Session, id: int):
+    async def get_teacher_id(db: Session, id: int):
         return db.query(Teacher).filter(Teacher.id == id).first()
     
     @staticmethod
-    async def create_teacher(self, db: Session, teacher: Teacher):
+    async def create_teacher(db: Session, teacher: Teacher):
         new_teacher = Teacher(**teacher.dict())
         db.add(new_teacher)
         db.commit()
@@ -20,7 +20,7 @@ class TeacherRepository:
         return new_teacher
     
     @staticmethod
-    async def update_teacher(self, db: Session, id: int, teacher: Teacher):
+    async def update_teacher(db: Session, id: int, teacher: Teacher):
         teacher_update = db.query(Teacher).filter(Teacher.id == id).first()
         update_data = teacher.dict(exclude_unset=True)
 
@@ -32,7 +32,7 @@ class TeacherRepository:
         return teacher_update
     
     @staticmethod
-    async def delete_teacher(self, db: Session, id: int):
+    async def delete_teacher(db: Session, id: int):
         teacher_delete = db.query(Teacher).filter(Teacher.id == id).first()
         db.delete(teacher_delete)
         db.commit()
