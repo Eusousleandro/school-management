e60a6537ac3db4dc8d0888e4f716d3dd7f4ab762
Leandro Sousa Silva <ordnael.sousass@gmail.com>
2026-04-17 10:33:18 -0300

    Modifitation in teacher routes.

--- src/routes/teacher.py
+++ src/routes/teacher.py
@@ -1,10 +1,11 @@
 from fastapi import APIRouter
 from controlleries.teacher import TeacherController
+from schemas.response.teacher_response import TeacherResponse
 
 router_teacher = APIRouter(prefix="/teachers", tags=["teachers"])
 
-router_teacher.get('/teachers', status_code=200)(TeacherController.get_teachers)
-router_teacher.get('/teacher/{id}', status_code=200)(TeacherController.get_teacher)
+router_teacher.get('/teachers', response_model=TeacherResponse, status_code=200)(TeacherController.get_teachers)
+router_teacher.get('/teacher/{id}', response_model=TeacherResponse, status_code=200)(TeacherController.get_teacher)
 router_teacher.post('/create/teacher', status_code=201)(TeacherController.create_teacher)
 router_teacher.put('/update/{id}', status_code=200)(TeacherController.update_teacher)
 router_teacher.delete('/delete/{id}', status_code=200)(TeacherController.delete_teacher)
