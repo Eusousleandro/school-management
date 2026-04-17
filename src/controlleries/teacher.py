ada4cafbcf2a937b0846efbfbeaa3b7943b5fd44
Leandro Sousa Silva <ordnael.sousass@gmail.com>
2026-04-17 15:58:50 -0300

    Refactoring the teacher controller, the user controller, and the login route.

--- src/controlleries/teacher.py
+++ src/controlleries/teacher.py
@@ -1,5 +1,6 @@
 from fastapi import Request, Response, Depends, status
 from sqlmodel import Session
+from config.auth.auth import get_current_user
 from config.database.session import get_db
 from mappers.teacher_mapper import to_teacher_response
 from repositories.teacher import TeacherRepository
@@ -11,27 +12,30 @@ service = TeacherService(repository)
 
 class TeacherController:
     async def get_teachers(request: Request, response: Response, 
-                           db: Session = Depends(get_db)):
-            teachers = await service.get_teachers(db)
-            return [to_teacher_response(teacher) for teacher in teachers]
+            db: Session = Depends(get_db), current_user = Depends(get_current_user)):
+
+        teachers = await service.get_teachers(db)
+        return [to_teacher_response(teacher) for teacher in teachers]
 
     async def get_teacher(request: Request, response: Response, 
-            id: int, db: Session = Depends(get_db)):
+            id: int, db: Session = Depends(get_db), current_user = Depends(get_current_user)):
         
-            teacher = await service.get_teacher_id(db=db, id=id)
-            return to_teacher_response(teacher)
+        teacher = await service.get_teacher_id(db=db, id=id)
+        return to_teacher_response(teacher)
 
     async def create_teacher(request: Request, response: Response,
-            teacher: TeacherCreate, db: Session = Depends(get_db)):
+            teacher: TeacherCreate, db: Session = Depends(get_db), 
+            current_user = Depends(get_current_user)):
         
         return await service.create_teacher(db=db, teacher=teacher)
 
     async def update_teacher(request: Request, id: int, teacher: TeacherUpdate, 
-                            db: Session = Depends(get_db)):
+            db: Session = Depends(get_db), current_user = Depends(get_current_user)):
         
         return await service.update_teacher(db=db, id=id, teacher=teacher)
 
     async def delete_teacher(request: Request, response: Response,
-            id: int, db: Session = Depends(get_db)):
+            id: int, db: Session = Depends(get_db),
+            current_user = Depends(get_current_user)):
 
         return await service.delete_teacher(db=db, id=id)
--- src/controlleries/user.py
+++ src/controlleries/user.py
@@ -1,5 +1,6 @@
 from fastapi import Depends, Request, Response, status
 from sqlmodel import Session
+from config.auth.auth import get_current_user
 from mappers.user_mapper import to_user_response
 from repositories.user import UserRepository
 from schemas.user import  UserCreate, UserUpdate
@@ -12,29 +13,31 @@ user_service = UserService(repository)
 
 class UserController:
     async def getUsers(request: Request, response: Response, 
-                       db: Session = Depends(get_db)):
+            db: Session = Depends(get_db), 
+            current_user = Depends(get_current_user)):
             
             users = await user_service.get_user(db)
             return [to_user_response(u) for u in users]
     
     async def getUserId(request: Request, response: Response, id: int, 
-                db: Session = Depends(get_db)):
+            db: Session = Depends(get_db), current_user = Depends(get_current_user)):
 
             user = await user_service.get_user_id(id=id, db=db)
             return to_user_response(user)
 
     async def createUser(request: Request, response: Response, user: UserCreate,
-                db: Session = Depends(get_db)):
+            db: Session = Depends(get_db), current_user = Depends(get_current_user)):
         
             user.password = hash_password(user.password)
             return await user_service.create_user(db=db, user=user)
 
     async def updateUser(request: Request, response: Response, id: int, 
-            user: UserUpdate, db: Session = Depends(get_db)):
+            user: UserUpdate, db: Session = Depends(get_db),
+            current_user = Depends(get_current_user)):
 
             return await user_service.update_user(db=db, id=id, user=user)
             
     async def deleteUser(request: Request, response: Response, id: int, 
-                   db: Session = Depends(get_db)):
+            db: Session = Depends(get_db), current_user = Depends(get_current_user)):
   
             return await user_service.delete_user(db=db, id=id)
