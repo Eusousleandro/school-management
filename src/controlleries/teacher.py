7bfda798250369d5cb2f57baac269c8c4c1bda4d
Leandro Sousa Silva <ordnael.sousass@gmail.com>
2026-04-17 10:30:04 -0300

    Modifitation in teacher controller.

--- src/controlleries/teacher.py
+++ src/controlleries/teacher.py
@@ -1,6 +1,7 @@
 from fastapi import Request, Response, Depends, status
 from sqlmodel import Session
 from config.database.session import get_db
+from mappers.teacher_mapper import to_teacher_response
 from repositories.teacher import TeacherRepository
 from schemas.teacher import TeacherCreate, TeacherUpdate
 from services.teacher import TeacherService
@@ -9,55 +10,28 @@ repository = TeacherRepository()
 service = TeacherService(repository)
 
 class TeacherController:
-    async def get_teachers(request: Request, response: Response, db: Session = Depends(get_db)):
-        try:
+    async def get_teachers(request: Request, response: Response, 
+                           db: Session = Depends(get_db)):
             teachers = await service.get_teachers(db)
-            response.status_code = status.HTTP_200_OK
-            return { 'Teachers:' if len(teachers) > 1 else 'Teachers:': teachers }
-        except Exception as error:
-            response.status_code = status.HTTP_500_INTERNAL_SERVER_ERROR
-            return { 'Error:': str(error) }
+            return [to_teacher_response(teacher) for teacher in teachers]
 
-    async def get_teacher(request: Request, response: Response, id: int, db: Session = Depends(get_db)):
-        try:
+    async def get_teacher(request: Request, response: Response, 
+            id: int, db: Session = Depends(get_db)):
+        
             teacher = await service.get_teacher_id(db=db, id=id)
-            response.status_code = status.HTTP_200_OK
-            return { 'Teacher:': teacher }
-        except Exception as error:
-            response.status_code = status.HTTP_500_INTERNAL_SERVER_ERROR
-            return { 'Error:': str(error) }
+            return to_teacher_response(teacher)
 
     async def create_teacher(request: Request, response: Response,
             teacher: TeacherCreate, db: Session = Depends(get_db)):
-        try:
-            teacher_create = request.json()
-            teacher = TeacherCreate(**teacher_create)
-            new_teacher = await service.create_teacher(db=db, teacher=teacher)
-            response.status_code = status.HTTP_201_CREATED
-            return { 'Teacher created with sucess:': str(new_teacher.id), 'Teacher:': new_teacher }
-        except Exception as error:
-            response.status_code = status.HTTP_500_INTERNAL_SERVER_ERROR
-            return { 'Error:': str(error) }
+        
+        return await service.create_teacher(db=db, teacher=teacher)
 
-    async def update_teacher(request: Request, response: Response,
-            id: int, teacher: TeacherUpdate, db: Session = Depends(get_db)):
-        try:
-            teacher_update = request.json()
-            teacher = TeacherUpdate(**teacher_update)
-            update_teacher = await service.update_teacher(db=db, id=id, teacher=teacher)
-            response.status_code = status.HTTP_500_INTERNAL_SERVER_ERROR
-            return { 'Teacher updated with secuss:': str(update_teacher.id), 'Teacher:': update_teacher }
-        except Exception as error:
-            response.status_code = status.HTTP_500_INTERNAL_SERVER_ERROR
-            return { 'Error:': str(error) }
+    async def update_teacher(request: Request, id: int, teacher: TeacherUpdate, 
+                            db: Session = Depends(get_db)):
+        
+        return await service.update_teacher(db=db, id=id, teacher=teacher)
 
     async def delete_teacher(request: Request, response: Response,
             id: int, db: Session = Depends(get_db)):
-        try:
-            teacher_delete = await service.delete_teacher(db=db, id=id)
-            response.status_code = status.HTTP_200_OK
-            return { 'Teacher deleted with sucess:': str(teacher_delete.id), 'Teacher:': teacher_delete }
-        except Exception as error:
-            response.status_code = status.HTTP_500_INTERNAL_SERVER_ERROR
-            return { 'Error:': str(error) }
 
+        return await service.delete_teacher(db=db, id=id)
